373dff91a5f243d3c492d181c1e93c881404fb2a
Aaron <aronsule@gmail.com>
2026-03-05 17:21:09 +0300

    Adding serializers and API views for task

---
+++ tasks/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = [
+            "id",
+            "title",
+            "description",
+            "due_date",
+            "priority",
+            "status",
+            "completed_at",
+            "created_at"
+        ]
